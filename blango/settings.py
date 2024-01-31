ac3ef1cd3d7a057ee4c065e9d6206a0ede2bf29a
Sushank Ghimire <sushankgghimire@gmail.com>
2024-01-31 16:26:08 +0000

    Finish db optimization

--- blango/settings.py
+++ blango/settings.py
@@ -49,10 +49,12 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        'debug_toolbar'
     ]
 
     MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -174,7 +176,10 @@ class Dev(Configuration):
         "handlers": ["console"],
         "level": "DEBUG",
     },
-}
+
+    
+    }
+    INTERNAL_IPS = ["192.168.10.93"]
 
 
 
